9457310e6aecab56a642cdd55ba5c7dcbb2ed8d5
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2022-08-18 18:15:32 +0900

    add passowrd display icon at UI

--- frontend/src/pages/admin/parts/LoginForm.tsx
+++ frontend/src/pages/admin/parts/LoginForm.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { Container, Stack, TextField, Button } from "@mui/material";
+import { Container, Stack, TextField, Button, InputAdornment, IconButton } from "@mui/material";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 import { isValidEmail } from "../../../libs/util/Email";
 import { RequiredErrorMessage } from "../../../libs/ErrorMessages";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 type AdminLoginFormProps = {
   input: LoginInput;
@@ -28,9 +29,13 @@ export default function AdminLoginForm(props: AdminLoginFormProps) {
     handleSubmit,
     formState: { errors },
   } = useForm<LoginInput>({ defaultValues: props.input });
+  const [showPassword, setShowPassword] = React.useState(false);
   const onSubmit: SubmitHandler<LoginInput> = (data) => {
     props.onSubmit(data);
   };
+  function handleClickShowPassword() {
+    setShowPassword(!(showPassword))
+  }
   const handleEmailValidation = (email: string) => {
     console.log("handleEmailValidation");
     const isValid = isValidEmail(email);
@@ -57,11 +62,24 @@ export default function AdminLoginForm(props: AdminLoginFormProps) {
           <TextField
             label="Password"
             fullWidth
-            type="password"
+            type={showPassword ? 'text' : 'password'}
             autoComplete="current-password"
             {...register("password", {
               required: { value: true, message: RequiredErrorMessage },
             })}
+            InputProps={{
+              endAdornment:
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={(e) => e.preventDefault()}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+            }}
             error={Boolean(errors.password)}
             helperText={errors.password && errors.password.message}
           />
